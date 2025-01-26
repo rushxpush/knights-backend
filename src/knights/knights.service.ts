cae12bcf03cd469319aaa0041fc50d6b1013f2f0
Rafael Garcia <rushxpush@gmail.com>
2025-01-26 17:14:28 -0300

    feat: add error handling to findOne in knightsService

--- src/knights/knights.service.ts
+++ src/knights/knights.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateKnightDto } from './dto/create-knight.dto';
 import { UpdateKnightDto } from './dto/update-knight.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Knight } from './models/knights.schema';
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 import { HeroesService } from 'src/heroes/heroes.service';
 import { CreateHeroDto } from 'src/heroes/dto/create-hero.dto';
 import { KnightsCalculationProvider } from './providers/knights-calculation.provider';
@@ -35,7 +35,15 @@ export class KnightsService {
   }
 
   async findOne(_id: string) {
+    if (!mongoose.Types.ObjectId.isValid(_id)) {
+      throw new HttpException('Formato de ID inválido', HttpStatus.BAD_REQUEST);
+    }
     const knight: Knight = await this.knightModel.findById({ _id }).lean();
+
+    if (!knight) {
+      throw new HttpException('Cavaleiro não encontrado', HttpStatus.NOT_FOUND);
+    }
+
     return {
       ...knight,
       experience: this.calcProvider.calculateExperience(knight),
